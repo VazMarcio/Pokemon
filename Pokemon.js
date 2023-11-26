b13704d7bb1b7b065505c2ee6772d1bdaef372ae
VazMarcio <githubmarcio@gmail.com>
2023-11-26 16:37:46 -0300

    Foco

---
+++ Pokemon.js
@@ -0,0 +1,24 @@
+let poochyena = {
+    nome: "Poochyena",
+    nivel: 2,
+    vidas: {atual: 13, total: 13}
+}
+let zigzagoon = {
+    nome: "Zigzagoon",
+    nivel: 2,
+    vidas: {atual: 13, total: 13}
+}
+let dragonite = {
+    nome: "Dragonite",
+    nivel: 5,
+    vidas: {atual: 25, total: 25}
+}
+let wurmple = {
+    nome: "Wurmple",
+    nive: 2,
+    vidas: {atual: 7, total: 14}
+}
+console.log(poochyena.nome + " Cadastrado com sucesso")
+console.log(zigzagoon.nome +  " Cadastrado com sucesso")
+console.log(dragonite.nome + " Cadastrado com sucesso")
+console.log(wurmple.nome + " Cadastrado com sucesso")
